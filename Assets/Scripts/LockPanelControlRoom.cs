cb890aaf7397449225d779a6beaf59942393aff4
dunjchz <dunja.parezanin@gmail.com>
2024-05-07 18:55:07 +0200

    Locket added to the control room

---
+++ Assets/Scripts/LockPanelControlRoom.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using TMPro;
+
+public class LockPanelControlRoom : MonoBehaviour
+{
+    
+    public TMP_InputField inputField1;
+    public TMP_InputField inputField2;
+    public TMP_InputField inputField3;
+
+    public TextMeshProUGUI errorMessage;
+    public GameObject lockPanel;
+    
+    
+
+    public void OnSubmitButtonPressed()
+    {
+        
+        if (inputField1.text == "4" && inputField2.text == "4" && inputField3.text == "4")
+        {
+
+            //Debug.Log("Code is correct, u have fuel now");
+            errorMessage.text = "Code correct! You have a fuel now.";
+            lockPanel.SetActive(false);
+            PlayerPrefs.SetInt("LockSolved", 1); 
+            
+        }
+        else
+        {
+            //Debug.Log("Code is incorrect!");
+            errorMessage.text = "Code incorrect! Try again.";
+        }
+    }
+
+}
